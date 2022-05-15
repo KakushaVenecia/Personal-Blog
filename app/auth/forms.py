2d48b802e2d9aa20fc31bf2b9212f4a5269413d7
KakushaVenecia <kakushavenecia@gmail.com>
2022-05-16 02:58:06 +0300

    Initial commit of the auth folder

---
+++ app/auth/forms.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField, SelectField, BooleanField, PasswordField
+from wtforms.validators import DataRequired ,Email
+
+class AdminForm(FlaskForm):
+    username = StringField('Your Username',validators=[DataRequired(),Email()])
+    password = PasswordField('Password',validators =[DataRequired()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
+
+
+class UserForm(FlaskForm):
+    username = StringField('Your Username',validators=[DataRequired(),Email()])
+    password = PasswordField('Password',validators =[DataRequired()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
+
+class SubscribeForm(FlaskForm):
+    email = StringField('Your Username',validators=[DataRequired(),Email()])
+    submit = SubmitField('Subscribe')
+
+
+
+
---
+++ app/auth/views.py
@@ -0,0 +1,11 @@
+# from urllib import response
+# from flask import render_template,request,redirect,url_for,abort,flash
+# from . import main
+# import json, requests
+# from app import auth
+
+
+
+# @auth.route('/admin')
+# def admin():
+#     return render_template('/admin.html')
